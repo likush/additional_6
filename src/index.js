247100039240fd04dcd9733e78e97bb810c9402c
lkush <l.kushnariova@yandex.ru>
2017-09-26 00:04:01 +0300

    First solution implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function zeros(expression) {
-  // your solution
+    let expressionParts = expression.split('*');
+    expressionParts = expressionParts.map(part => {
+        let n = part.replace(/!/g, '');
+        let gap = part.match(/!/g).length;
+        return nFactorial(n, gap)
+    });
+
+    let total = 1;
+    expressionParts.forEach(part => {
+        total *= part;
+    });
+
+    const matchedZeros = String(total).match(/0*$/g);
+    return matchedZeros[0].length;
+};
+
+function nFactorial(n, gap) {
+    let ni = n - gap, result = n;
+
+    while (ni >= gap) {
+        result *= ni;
+        ni -= gap;
+    }
+
+    return result;
 }
